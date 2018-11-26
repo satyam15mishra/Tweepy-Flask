f4d3b6faf684258460958235d7e8b0aa6bdfbb0f
Satyam Mishra <satyamgusmishra@gmail.com>
2018-11-26 10:38:59 +0530

    Update myapp.py

--- Tweepy/myapp.py
+++ Tweepy/myapp.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, session, g, flash
 import tweepy 
+import requests
+
+
+#############################################################################################
+################################  TWEEPY SETUP  #################################################
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+##############################################################################################
 
 app = Flask(__name__) 
  
@@ -7,21 +22,13 @@ app = Flask(__name__)
 def page():
     return render_template('home.html')
  
+
+#---------------T W I T T E R  A P I---------------
+#---------------GETTING TWEET SPECIFIC TO USERNAME---------------
 @app.route('/send', methods=['GET', 'POST'])
 def send():
 	if request.method=='POST':
 		user_interest = request.form['user_interest']
-		
-		consumer_key = 'HSUdBnSRVroa1R2O3DM2bnAY7'
-		consumer_secret = '7piXeV9dYG9vdokLMqVgtgzXVHlA24U0pEiRn8BHLjRbTd2mel'
-		access_token = '780954917515124736-6ucBM7w84wWkoCVoEjG9D3OGDF88HE1'
-		access_token_secret = 'FqoGm7w3SnChaXz2BLrgyk3hneTGywnXGj4rZgZrvE99H'
-
-		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-		auth.set_access_token(access_token, access_token_secret)
-
-		api = tweepy.API(auth)
 
 		usr = api.get_user(user_interest)
 		a = usr.followers_count
@@ -33,6 +40,16 @@ def send():
 
 	return render_template('home.html')
 
+#---------------GETTING TWEETS SPECIFIC TO HASHTAG---------------
+@app.route('/hash', methods=['GET', 'POST'])
+def hash():
+	if request.method=='POST':
+		hashtag = request.form['hashtag']
+		h = tweepy.Cursor(api.search, q = hashtag, count = "10", lang = "en", since = "2018-11-11").items()
+		return render_template('hash.html', h = h, hashtag = hashtag)
+
+	return render_template('map.html')
+
 if (__name__ == "__main__"):
 	app.debug = True
-	app.run()
+	app.run(threaded = True)
